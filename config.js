a4a6b1f363a28a58e313d5f2d06f46d3bfbce4b5
vergris57 <jalal.gassi.pro@gmail.com>
2026-08-03 10:30:19 +0200

    Add configuration for FollowMatch

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuration FollowMatch
+window.FM_CONFIG = {
+  SUPABASE_URL: "https://ehoaqwounccjwszfvnef.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_V2neeHiDA_qrULp1Tw9DuA_icGqSySN"
+};
+
